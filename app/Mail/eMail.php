33ac425aedea825d999e4a3b307fd4c1e5c65391
andrepaes12 <aluispaes@gmail.com>
2022-08-04 19:29:37 -0300

    enviando email com SendGrid

---
+++ app/Mail/eMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class eMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $this->subject('Assunto do email');
+        $this->to('aluispaes@gmail.com', 'Andre Paes');
+        return $this->view('mail');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MailController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,7 @@ Route::resource('users', UserController::class);
 Route::resource('addresses', AddressController::class);
 Route::resource('posts', PostController::class);
 Route::resource('categories', CategoryController::class);
+Route::resource('mails', MailController::class);
 
 Route::get('/', function () {
     return view('welcome');
